4a7336680e9b8035863dfb8f969f47749a769911
NadyaSakh <Nadyasaxorova@mail.ru>
2017-11-26 16:57:43 +1000

    Update project_6.py

--- project_6.py
+++ project_6.py
@@ -11,4 +11,3 @@ obj = json.loads(response.read().decode())
 print(obj)
 print("longitude = ", obj['iss_position']['longitude'])
 print("latitude = ",obj['iss_position']['latitude'])
-#{'timestamp': 1511595604, 'message': 'success', 'iss_position': {'latitude': '51.4139', 'longitude': '-127.4700'}}
